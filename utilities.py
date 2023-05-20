95bbd852321335e492b90a0095c5194734cb84de
Aliaa Mohammed <110140230+Aliaa-Elbishlawy@users.noreply.github.com>
2023-05-20 19:28:33 +0300

    utilities.py

--- utilities.py
+++ utilities.py
@@ -41,4 +41,129 @@ def getUpcomingEmptyRow(board, c):
             return row
     return -1
 
+def normalMinimax(board, depth, maximizingPlayer):
+    if depth == 0 or isTerminalState(board):
+        if isTerminalState(board):
+            if isWinningTurn(board, 1):
+                return float("-inf")
+            elif isWinningTurn(board, 2):
+                return float("inf")
+            else:
+                return 0
+        else:
+            return boardEvaluation(board, 2)
+
+    if maximizingPlayer:
+        maxEval = float("-inf")
+        for col in getPossibleMoves(board):
+            row = getUpcomingEmptyRow(board, col)
+            board_copy = board.copy()
+            board_copy[row][col] = 2
+            eval = normalMinimax(board_copy, depth - 1, False)
+            maxEval = max(maxEval, eval)
+        return maxEval
+    else:
+        minEval = float("inf")
+        for col in getPossibleMoves(board):
+            row = getUpcomingEmptyRow(board, col)
+            board_copy = board.copy()
+            board_copy[row][col] = 1
+            eval = normalMinimax(board_copy, depth - 1, True)
+            minEval = min(minEval, eval)
+        return minEval
+
+
+def refinedMinimax(board, depth, alpha, beta, maximizingPlayer):
+    if depth == 0 or isTerminalState(board):
+        if isTerminalState(board):
+            if isWinningTurn(board, 1):
+                return float("-inf")
+            elif isWinningTurn(board, 2):
+                return float("inf")
+            else:
+                return 0
+        else:
+            return boardEvaluation(board, 2)
+
+    if maximizingPlayer:
+        maxEval = float("-inf")
+        for col in getPossibleMoves(board):
+            row = getUpcomingEmptyRow(board, col)
+            board_copy = board.copy()
+            board_copy[row][col] = 2
+            eval = refinedMinimax(board_copy, depth - 1, alpha, beta, False)
+            maxEval = max(maxEval, eval)
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
+        return maxEval
+    else:
+        minEval = float("inf")
+        for col in getPossibleMoves(board):
+            row = getUpcomingEmptyRow(board, col)
+            board_copy = board.copy()
+            board_copy[row][col] = 1
+            eval = refinedMinimax(board_copy, depth - 1, alpha, beta, True)
+            minEval = min(minEval, eval)
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        return minEval
+
+
+def isWinningTurn(board, circle):
+    for i in range(3):
+        for j in range(3, 4):
+            if board[i][j] == circle and board[i + 1][j - 1] == circle and board[i + 2][j - 2] == circle and \
+                    board[i + 3][j - 3] == circle:
+                return True
+
+    for j in range(4):
+        for i in range(6):
+            if board[i][j] == circle and board[i][j + 1] == circle and board[i][j + 2] == circle and board[i][
+                j + 3] == circle:
+                return True
+
+    for j in range(7):
+        for i in range(3):
+            if board[i][j] == circle and board[i + 1][j] == circle and board[i + 2][j] == circle and board[i + 3][
+                j] == circle:
+                return True
+    for j in range(4):
+        for i in range(3):
+            if board[i][j] == circle and board[i + 1][j + 1] == circle and board[i + 2][j + 2] == circle and \
+                    board[i + 3][j + 3] == circle:
+                return True
+
+    return False
+
+
+def evaluateSum(window, thisPlayerCircle):
+    opponentCircle = 3 - thisPlayerCircle
+    thisPlayerSum = 0
+    opponentSum = 0
+    emptySum = 0
+
+    for cell in window:
+        if cell == thisPlayerCircle:
+            thisPlayerSum += 1
+        elif cell == opponentCircle:
+            opponentSum += 1
+        else:
+            emptySum += 1
+
+    nodeSum = 0
+
+    if thisPlayerSum == 3 and emptySum == 1:
+        nodeSum += 10
+    elif thisPlayerSum == 2 and emptySum == 2:
+        nodeSum += 6
+    elif thisPlayerSum == 4:
+        nodeSum += 1000000
+    if opponentSum == 3 and emptySum == 1:
+        nodeSum -= 10
+
+    return nodeSum
+
+
 
